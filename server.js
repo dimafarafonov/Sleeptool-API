0e52275712aaec410ac633cacfa6521e1b95a394
DiiF <dimafarafonovzt@gmail.com>
2023-11-23 22:24:35 +0200

    Passing arguments

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var schema = buildSchema(`
     hello: String
     systemAlert: Int!
     listType: [String]
-    nullableReturn: Float! 
+    nullableReturn: Float!
+    rollDice(numDice: Int!, numSides: Int): [Int] 
   }
 `);
 
@@ -21,16 +22,20 @@ var root = {
     return 1; // cannot be null
   },
   listType: () => {
-    const map = [
-      1,2,3,4,5
-    ];
+    const map = [1, 2, 3, 4, 5];
 
-    return map
+    return map;
   },
   nullableReturn: () => {
-    return 1.245325 // cannot be null, but we are fine with float's 
-  }
-
+    return 1.245325; // cannot be null, but we are fine with float's
+  },
+  rollDice: ({ numDice, numSides }) => {
+    var output = [];
+    for (let i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  },
 };
 // stopping on basic types
 var app = express();
